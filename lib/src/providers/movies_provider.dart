c3a44b92db8e046b62292fe12665d15c64a527d6
javi95z <javi.95.zgz@gmail.com>
2019-09-21 12:40:31 +0200

    108 Creando un Stream de peliculas

--- lib/src/providers/movies_provider.dart
+++ lib/src/providers/movies_provider.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:movies_flutter_app/src/models/movie_model.dart';
@@ -6,6 +7,17 @@ class MoviesProvider {
   String _apiKey = 'fd94b9a423e09c3717c36d73af49f46b';
   String _url = 'api.themoviedb.org';
   String _language = 'es-ES';
+
+  int _popularPage = 0;
+  List<Movie> _popular = new List();
+  final _popularStreamController = StreamController<List<Movie>>.broadcast();
+  
+  Function(List<Movie>) get popularSink => _popularStreamController.sink.add;
+  Stream<List<Movie>> get popularStream => _popularStreamController.stream;
+  void disposeStreams() {
+    _popularStreamController?.close();
+  }
+
 
   Future<List<Movie>> _proccessResponse(Uri url) async {
     final response = await http.get(url);
@@ -21,8 +33,15 @@ class MoviesProvider {
   }
 
   Future<List<Movie>> getPopular() async {
-    final url = Uri.https(
-        _url, '3/movie/popular', {'api_key': _apiKey, 'language': _language});
-    return await _proccessResponse(url);
+    _popularPage++;
+    final url = Uri.https(_url, '3/movie/popular', {
+      'api_key': _apiKey,
+      'language': _language,
+      'page': _popularPage.toString()
+    });
+    final resp = await _proccessResponse(url);
+    _popular.addAll(resp);
+    popularSink(_popular);
+    return resp;
   }
 }
