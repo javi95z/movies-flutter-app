3776bb4086ccde09a577174226227b495461995f
javi95z <javi.95.zgz@gmail.com>
2019-09-29 17:31:54 +0200

    121 BuildSuggestions y BuildResults

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 
 class DataSearch extends SearchDelegate {
+  String selection = '';
+  final movies = [
+    'Spiderman',
+    'Aquaman',
+    'Batman',
+    'Superman',
+    'Ironman',
+    'Ironman 2',
+    'Ironman 3',
+    'Shazam!',
+    'Captain America',
+  ];
+  final recentMovies = ['Spiderman', 'Captain America'];
+
   @override
   List<Widget> buildActions(BuildContext context) {
     return [
@@ -24,11 +38,35 @@ class DataSearch extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    return Container();
+    return Center(
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        color: Colors.blueAccent,
+        child: Text(selection),
+      ),
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Container();
+    final suggestedList = (query.isEmpty)
+        ? recentMovies
+        : movies
+            .where((p) => p.toLowerCase().startsWith(query.toLowerCase()))
+            .toList();
+    return ListView.builder(
+      itemCount: suggestedList.length,
+      itemBuilder: (context, i) {
+        return ListTile(
+          leading: Icon(Icons.movie),
+          title: Text(suggestedList[i]),
+          onTap: () {
+            selection = suggestedList[i].toString();
+            showResults(context);
+          },
+        );
+      },
+    );
   }
 }
